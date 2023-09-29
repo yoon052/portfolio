dfa2464d416b3fc5771a0a738360abfc319fa53c
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-29 23:58:44 +0900

    feat: JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -13,6 +13,15 @@ document.addEventListener("scroll", () => {
 const home = document.querySelector(".home__container");
 const homeHeight = home.offsetHeight;
 document.addEventListener("scroll", () => {
-  console.log((home.style.opacity = 1 - window.scrollY / homeHeight));
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
 });

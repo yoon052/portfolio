61403815b70f99fc6c996639bcfea48966b1ac1a
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-30 15:01:50 +0900

    feat: observer

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,10 +1,3 @@
-// 구현 계획
-// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
-// 2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야 한다.
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
-// 보여지는 섹션:
-// - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
-// - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막 섹션을 선택
 const sectionIds = [
   "#home",
   "#about",
@@ -13,13 +6,20 @@ const sectionIds = [
   "#testimonial",
   "#contact",
 ];
+
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
-  document.querySelector(`[href="${id}]`)
+  document.querySelector(`[href="${id}"]`)
 );
-const visibleSections = sectionIds.map(() => false);
 
-const options = {};
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
+
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -31,18 +31,24 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
-  console.log(visibleSections);
-  console.log("무조건 라스트 섹션!!", selectLastOne);
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }

9bd7f37c351740a9eb9cb60892e6fbbdf0e7f8ba
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-09-30 10:09:23 +0900

    feat: projects filtering

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+categories.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+
+  // Actuve 매뉴룰 재설정
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  event.target.classList.add("category--selected");
+
+  // 프로젝트 필터링
+  projectsContainer.classList.add("anim-out");
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+});
